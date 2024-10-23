ba309a41efa6b3cf310ad9c3b863d3404f2f5c69
zhufucdev <1095291434@qq.com>
2024-10-23 09:28:43 +0800

    :art: better naming

--- interactive_encode.m
+++ interactive_encode.m
@@ -2,10 +2,10 @@ text = input('Text to encode:', 's');
 file = input('Path to encoding image:', 's');
 
 im = imread(file);
-encode_buffer = encode(text, im);
+encodeBuffer = encode(text, im);
 
 subplot(1, 2, 1);
 imshow(im);
 
 subplot(1, 2, 2);
-imshow(encode_buffer);
+imshow(encodeBuffer);
